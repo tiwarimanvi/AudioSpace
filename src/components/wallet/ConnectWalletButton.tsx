8bd95896fffca20e0a0cd6c30f664565e8420985
tiwarimanvi <manvitiwari029@gmail.com>
2024-04-20 21:46:20 +0530

    connect wallet part almost done

---
+++ src/components/wallet/ConnectWalletButton.tsx
@@ -0,0 +1,39 @@
+import { TonConnectUIProvider, THEME } from '@tonconnect/ui-react';
+
+const ConnectWalletButton = ({ children }) => {
+  return (
+    <TonConnectUIProvider
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+      uiPreferences={{ theme: THEME.DARK }}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "safepalwallet",
+            name: "SafePal",
+            imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+            aboutUrl: "https://www.safepal.com/download",
+            jsBridgeKey: "safepalwallet",
+            platforms: ["ios", "android", "chrome", "firefox"]
+          },
+          {
+            appName: "tonwallet",
+            name: "TON Wallet",
+            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+            aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+            universalLink: "https://wallet.ton.org/ton-connect",
+            jsBridgeKey: "tonwallet",
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["chrome", "android"]
+          }
+        ]
+      }}
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
+      }}
+    >
+      {children}
+    </TonConnectUIProvider>
+  );
+};
+
+export default ConnectWalletButton;
